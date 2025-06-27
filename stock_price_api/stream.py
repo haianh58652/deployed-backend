6e9ad799a44e7c013526b266ab0757c30cba4eef
haianh63 <127457276+haianh63@users.noreply.github.com>
2025-06-27 23:04:56 +0700

    Update stream.py

--- stock_price_api/stream.py
+++ stock_price_api/stream.py
@@ -24,14 +24,11 @@ def getError(error):
 	print(error)
 	
 def get_data_stream():
-	selected_channel = "X-TRADE:ALL"
-	mm = MarketDataStream(config, MarketDataClient(config))
-	mm.start(get_market_data, getError, selected_channel)
-	message = None
-	while message != "exit()":
-		message = input(">> ")
-		if message is not None and message != "" and message != "exit()":
-			mm.swith_channel(message)
+    selected_channel = "X-TRADE:ALL"
+    mm = MarketDataStream(config, MarketDataClient(config))
+    mm.start(get_market_data, getError, selected_channel)
+    event = threading.Event()  # Tạo sự kiện chờ
+    event.wait()
 
 
 def simulate_get_data():
